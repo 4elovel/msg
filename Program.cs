c7325ba3651153b583afe49f2c81939e507ba811
4elovel <marcynakvova007@gmail.com>
2023-05-08 11:50:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SimpleProject
+{
+    public class DllImportExample
+    {
+        [DllImport("User32.dll", ExactSpelling = true)]
+        public static extern int MessageBoxA(IntPtr hWnd, string text, string caption, uint type);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            DllImportExample.MessageBoxA(IntPtr.Zero, "Повідомлення у форматі Unicode успішно виведено", "Повідомлення", 0);
+        }
+    }
+}
